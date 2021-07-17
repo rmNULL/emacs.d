2096bf08794b596eae2b34d5f67acd265484a7f1
rmnull <rmnull@users.noreply.github.com>
2021-07-17 08:41:55 +0530

    calendar

--- lisp/conf/iduh-init-visual-pleasures.el
+++ lisp/conf/iduh-init-visual-pleasures.el
@@ -1,18 +1,18 @@
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
-(require 'calendar)
-(setq-default
- calendar-week-start-day 6
- scroll-preserve-screen-position 'always
- inhibit-startup-screen t)
+(use-package calendar
+  :config
+  (setq-default
+   calendar-week-start-day 6
+   scroll-preserve-screen-position 'always
+   inhibit-startup-screen t))
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (line-number-mode t)
 (column-number-mode t)
-
 (setq frame-title-format
       '(:eval (if (boundp 'server-name)
                   (list server-name " --- %b")
