e0135c00c55c877ec9684a6c900152db3f01807c
rmnull <rmnull@users.noreply.github.com>
2021-08-04 09:16:25 +0530

    lsp tsserver log write to /tmp/. electric pair mode and show matching paren.

--- custom-config.el
+++ custom-config.el
@@ -3,8 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-
+ '(electric-pair-mode t)
+ '(lsp-clients-typescript-server-args '("--stdio" "--tsserver-log-file" "/tmp/tsserver.log"))
+ '(show-paren-mode t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -12,10 +13,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(rainbow-delimiters-base-face ((t (:inherit nil))))
- '(rainbow-delimiters-depth-1-face
-   ((t (:inherit rainbow-delimiters-base-face :foreground "#df9326"))))
- '(rainbow-delimiters-depth-2-face
-   ((t (:inherit rainbow-delimiters-base-face :foreground "#e6e13d"))))
+ '(rainbow-delimiters-depth-1-face ((t (:inherit rainbow-delimiters-base-face :foreground "#df9326"))))
+ '(rainbow-delimiters-depth-2-face ((t (:inherit rainbow-delimiters-base-face :foreground "#e6e13d"))))
  '(rainbow-delimiters-depth-3-face ((t (:foreground "#de392e"))))
  '(rainbow-delimiters-depth-4-face ((t (:foreground "#37cb39"))))
  '(rainbow-delimiters-depth-5-face ((t (:foreground "#3d9dce"))))
