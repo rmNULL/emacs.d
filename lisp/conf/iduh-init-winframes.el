c87a934303ed3b3804873be2b17cc7c1bc1914d2
rmnull <rmnull@users.noreply.github.com>
2021-08-15 00:58:05 +0530

    use switch-window package.

--- lisp/conf/iduh-init-winframes.el
+++ lisp/conf/iduh-init-winframes.el
@@ -9,4 +9,17 @@
   :config
   (winner-mode 1))
 
+(use-package switch-window
+  :bind
+  ("C-x o" . switch-window)
+  ("C-x 1" . switch-window-then-maximize)
+  ("C-x 2" . switch-window-then-split-below)
+  ("C-x 3" . switch-window-then-split-right)
+  ("C-x 0" . switch-window-then-delete)
+  ("C-x 4 d" . switch-window-then-dired)
+  ("C-x 4 f" . switch-window-then-find-file)
+  ("C-x 4 0" . switch-window-then-kill-buffer)
+  :config
+  (setq switch-window-shortcut-style 'qwerty))
+
 (provide 'iduh-init-winframes)
