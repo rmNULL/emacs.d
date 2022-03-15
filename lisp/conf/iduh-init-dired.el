f8726d50c41e1975a79a833a14ed4a2f6798c613
rmnull <rmnull@users.noreply.github.com>
2022-03-15 15:27:53 +0530

    dired-hist

--- lisp/conf/iduh-init-dired.el
+++ lisp/conf/iduh-init-dired.el
@@ -3,4 +3,15 @@
   :custom
   (dired-listing-switches "-lhA --group-directories-first"))
 
+(use-package dired-hist
+  :straight (dired-hist
+             :type git
+             :host github
+             :repo "karthink/dired-hist")
+  :bind (:map dired-mode-map
+         ("l" . 'dired-hist-go-back)
+         ("r" . 'dired-hist-go-forward))
+  :config
+  (dired-hist-mode))
+
 (provide 'iduh-init-dired)
