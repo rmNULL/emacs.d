424cba79fa893f7e17f8c4dcd739850e307160df
rmnull <rmnull@users.noreply.github.com>
2022-04-14 00:25:05 +0530

    dired jump gets a new shortcut.

--- lisp/conf/iduh-init-dired.el
+++ lisp/conf/iduh-init-dired.el
@@ -1,18 +1,21 @@
-(use-package dired
-  :straight nil
-  :custom
-  (dired-listing-switches "-lhA --group-directories-first"))
-
 (use-package dired-hist
   :straight (dired-hist
              :type git
              :host github
              :repo "karthink/dired-hist")
-  :bind (:map dired-mode-map
-         ("l" . 'dired-hist-go-back)
-         ("r" . 'dired-hist-go-forward))
+  :bind
+  (:map dired-mode-map
+        ("l" . 'dired-hist-go-back)
+        ("r" . 'dired-hist-go-forward))
   :config
   (dired-hist-mode))
+
+(use-package dired
+  :straight nil
+  :bind
+  ("C-c C-j" . dired-jump)
+  :custom
+  (dired-listing-switches "-lhA --group-directories-first"))
 
 (use-package dired-x
   :straight nil
