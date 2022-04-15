60b94333456272c61a20aeb24a040cb3760b89a8
rmnull <rmnull@users.noreply.github.com>
2022-04-15 11:11:25 +0530

    new shortcuts for  easy-kill and undo operations.

--- lisp/conf/iduh-init-editing.el
+++ lisp/conf/iduh-init-editing.el
@@ -37,15 +37,19 @@
   (tiny :type git
         :host github
         :repo "abo-abo/tiny")
-  :config
-  (describe-key (kbd "C-;"))
   :bind
   ("C-;" . tiny-expand))
 
 (use-package easy-kill
+  :bind
+  ("Ω" . easy-mark)
+  ([remap kill-ring-save] . easy-kill)
+  ([remap mark-sexp] . easy-mark)
   :config
-  (global-set-key [remap kill-ring-save] 'easy-kill)
-  (global-set-key [remap mark-sexp] 'easy-mark))
+  ;; just unset usable ones, ignore lengthy bindings
+  (let ((keys '("C-M-h" "M-?" "M-@")))
+    (dolist (key keys)
+      (unbind-key key))))
 
 (use-package ws-butler
   :diminish
@@ -58,18 +62,29 @@
   :straight (undo-tree :type git
                        :host gitlab
                        :repo "tsc25/undo-tree")
+  :bind
+  (:map undo-tree-map
+        ("Υ" . undo-tree-undo)
+        ("C-υ" . undo-tree-visualize)
+        ("Ρ" . undo-tree-redo))
   :custom
   ;; in Bytes
   (undo-tree-limit (* 4 1024 1024))
   (undo-tree-strong-limit (* 8 1024 1024))
   (undo-tree-history-directory-alist
-   `((".*.gpg" .  "/dev/null")
-     (".*straight-stderr-.*" .  "/dev/null")
-     (".*.~undo-tree~" .  "/dev/null")
-     (".*" .  ,iduh-stray-files-undo-tree-directory)))
+   `((".*.gpg" . "/dev/null")
+     (".*straight-stderr-.*" . "/dev/null")
+     (".*.~undo-tree~" . "/dev/null")
+     (".*.gz" . "/dev/null")
+     (".*.xz" . "/dev/null")
+     (".*" . ,iduh-stray-files-undo-tree-directory)))
   (undo-tree-visualizer-timestamps t)
   :config
   (add-to-list 'undo-tree-incompatible-major-modes #'inferior-python-mode)
+  :init
+  ;; (let ((keys '("C-/" "C-_" "C-?" "M-_" "M-_" "C-x u")))
+  ;;   (dolist (key keys)
+  ;;     (unbind-key key)))
   (global-undo-tree-mode))
 
 (provide 'iduh-init-editing)
