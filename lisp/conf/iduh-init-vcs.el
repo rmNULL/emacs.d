36ab4c588e11ac31984cadc5f996a61cac998836
rmnull <rmnull@users.noreply.github.com>
2022-04-28 22:20:04 +0530

    git-modes

--- lisp/conf/iduh-init-vcs.el
+++ lisp/conf/iduh-init-vcs.el
@@ -1,7 +1,9 @@
 (use-package magit
   :bind (("<f7>" . magit-status)
          ("<f8>" . magit-dispatch)))
-(use-package git-timemachine)
-(use-package gitignore-mode)
+(use-package git-timemachine
+  :straight t)
+(use-package git-modes
+  :straight t)
 
 (provide 'iduh-init-vcs)
