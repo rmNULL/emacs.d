9d164b3e719b1ba40087512dbd78ef5f6ab7d3b9
rmnull <rmnull@users.noreply.github.com>
2022-10-30 10:22:35 +0530

    new pkg: mpdel

--- lisp/conf/iduh-init-media.el
+++ lisp/conf/iduh-init-media.el
@@ -11,6 +11,25 @@
   (:map helm-command-map
         ("u" . helm-mpd)))
 
+(use-package mpdel
+  :straight t
+  :init
+  (defun iduh/mpdel-do-delete (arg)
+    (interactive "P")
+    (debug)
+    (tablist-do-delete t))
+  :bind
+  (:map mpdel-playlist-mode-map
+        ("<deletechar>" . iduh/mpdel-do-delete)
+        ("C-k" . iduh/mpdel-do-delete)
+        ("C-K" . iduh/mpdel-do-delete))
+  (:map mpdel-core-map
+        ("~" . mpdel-browser-open)
+        ("0" . mpdel-browser-open)
+        ("1" . mpdel-playlist-open)
+        ("2" . mpdel-core-open-artists)
+        ("3" . mpdel-core-open-albums)))
+
 (provide 'iduh-init-media)
 
 ;;; iduh-init-media.el ends here
