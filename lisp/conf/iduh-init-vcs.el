495960fb10aa802bf22d3246d9689a4cf5195e19
rmnull <rmnull@users.noreply.github.com>
2022-11-02 07:47:36 +0530

    git-timemachine repo moved to codeberg

--- lisp/conf/iduh-init-vcs.el
+++ lisp/conf/iduh-init-vcs.el
@@ -2,7 +2,12 @@
   :bind (("<f7>" . magit-status)
          ("<f8>" . magit-dispatch)))
 (use-package git-timemachine
-  :straight t)
+  :straight
+  (git-timemachine
+   :type git
+   :host "https://codeberg.org/"
+   :repo "pidu/git-timemachine"))
+
 (use-package git-modes
   :straight t)
 
