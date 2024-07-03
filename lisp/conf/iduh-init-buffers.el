75138465580150a6c1c53183d4a457e190388ac9
rmnull <rmnull@users.noreply.github.com>
2024-07-03 23:32:46 +0530

    conf/buffers: use kmonad keys, and for encryption use the default rmnull key

--- lisp/conf/iduh-init-buffers.el
+++ lisp/conf/iduh-init-buffers.el
@@ -2,13 +2,21 @@
 
 (require 'iduh/buffers)
 
-(global-set-key (kbd "⁴") 'save-buffer)
-(global-set-key (kbd "Χ") 'iduh/kill-or-bury-current-buffer)
-(global-set-key (kbd "Ο") 'iduh/switch-previous-user-buffer)
-(global-set-key (kbd "ο") 'iduh/switch-next-user-buffer)
+(global-set-key (kbd "C-c z v") 'save-buffer)
+(global-set-key (kbd "C-c z x") 'iduh/kill-or-bury-current-buffer)
+(global-set-key (kbd "C-c z O") 'iduh/switch-previous-user-buffer)
+(global-set-key (kbd "C-c z o") 'iduh/switch-next-user-buffer)
+(global-set-key (kbd "<f5>") 'revert-buffer-quick)
 
 (setq initial-scratch-message "")
 ;; (setq initial-buffer-choice "/tmp/hi.org")
+
+(use-package epa
+  :straight nil
+  :demand t
+  :custom
+  (epa-file-select-keys 'silent)
+  (epa-file-encrypt-to '("rmnull@eos")))
 
 (provide 'iduh-init-buffers)
 
