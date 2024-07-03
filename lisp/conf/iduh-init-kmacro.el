4776210a7ac373ae158f568ca900b28ff6c35616
rmnull <rmnull@users.noreply.github.com>
2024-07-03 23:36:57 +0530

    conf/kmacro: macro to insert daily timestamp in org-mode, not yet bound to any key

--- lisp/conf/iduh-init-kmacro.el
+++ lisp/conf/iduh-init-kmacro.el
@@ -9,5 +9,9 @@
   (:map kmacro-keymap
         ("I" . kmacro-insert-macro)))
 
+(fset 'iduh/daily-task-timestamp-insert-in-my-todo-macro
+   (kmacro-lambda-form [?\C-e C-return ?\C-u ?\C-c ?. return ? ] 0 "%d"))
+
+
 (provide 'iduh-init-kmacro)
 ;;; iduh-init-kmacro.el ends here
