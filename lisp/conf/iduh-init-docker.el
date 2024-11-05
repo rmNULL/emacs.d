7df8a7b0ff2abb5900fb6f1610d74c5b444c46f3
rmnull <rmnull@users.noreply.github.com>
2024-11-05 19:49:33 +0530

    conf/docker: docker pass default run arguments to a container, an example is setup so that its just easier to copy. I don't really use that values for debian :)

--- lisp/conf/iduh-init-docker.el
+++ lisp/conf/iduh-init-docker.el
@@ -7,6 +7,10 @@
   ("C-c r" . docker)
   :custom
   (docker-image-default-sort-key '("Created" . t))
-  (docker-container-default-sort-key '("Created" . t)))
+  (docker-container-default-sort-key '("Created" . t))
+  ;;; dummy holder, replace with actual values as required
+  (docker-image-run-custom-args `(("debian" ("-e SECRET_KEY=MMMV-test-prod-env"
+                                             "-p 13535:80"
+                                             ,@docker-image-run-default-args)))))
 
 (provide 'iduh-init-docker)
